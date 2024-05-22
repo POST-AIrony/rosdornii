f00e69667ae541c413211a52ef10d54058e14e80
Леонид Чесников <leonid.chesnikov@gmail.com>
2024-05-22 12:10:27 +0300

    fix

--- backend/models/database.py
+++ backend/models/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost/rosdornii"
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@postgres/rosdornii"
 engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
